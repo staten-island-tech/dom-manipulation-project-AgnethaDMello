e7f6146b48ce0fdda7cb78612da958f6f6f26345
AgnethaDMello <agnethad@nycstudents.net>
2021-11-08 13:58:04 -0500

    Edit 3

--- java.js
+++ java.js
@@ -1,3 +1,6 @@
 const DOMSelectors = {
-
+    title: document.getElementById("title"),
+    artist: document.getElementById("artist"),
+    url: document.getElementById("url"),
+    submit: Document.querySelector(".btn")
 }
