0ff69d169918f51e28b229daef29f69275082980
AgnethaDMello <agnethad@nycstudents.net>
2021-11-09 13:58:05 -0500

    Edit 4 (send help)

--- java.js
+++ java.js
@@ -2,5 +2,20 @@ const DOMSelectors = {
     title: document.getElementById("title"),
     artist: document.getElementById("artist"),
     url: document.getElementById("url"),
-    submit: Document.querySelector(".btn")
+    submit: Document.querySelector(".btn"),
 }
+
+document.getElementById("form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    let album = {};
+    album.title = document.getElementById("title").value;
+    album.artist = document.getElementById("artist").value;
+    album.url = document.getElementById("url").value;
+    console.log(album);
+});
+
+DOMSelectors.submit.addEventListener("click", function () {
+    let titleInput = DOMSelectors.title.value;
+    DOMSelectors.submit.insertAdjacentHTML("beforeend", `<p> ${titleInput}</p>`);
+    DOMSelectors.titleInput.value = "";
+})
